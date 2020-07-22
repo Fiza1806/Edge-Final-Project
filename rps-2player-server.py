3bc615d977733f4a13eaf20e71a0ba5089fd6d31
Adel Fergatova <68508788+adelfergatova@users.noreply.github.com>
2020-07-22 13:23:29 +0600

    Update rps-2player-server.py

--- rps-2player-server.py
+++ rps-2player-server.py
@@ -6,9 +6,9 @@ s.listen()
 
 s1, addr1 = s.accept()
 
-s1.sendall("Hello! Welcome to our game! Waiting for a partner to connect. Please, wait)".encode())
+s1.sendall("Hello! Welcome to our game!\n".encode())
 
-s1.sendall("What is your username?".encode())
+s1.sendall("What is your username? Please enter:  ".encode())
 
 data = s1.recv(1024)
 
@@ -20,7 +20,7 @@ s2, addr2 = s.accept()
 
 s2.sendall("Hello! Welcome to our game! You are going to play with ".encode()+ name1.encode())
 
-s2.sendall("What is your username?".encode())
+s2.sendall("What is your username? Please enter:  ".encode())
 
 data = s2.recv(1024)
 
@@ -33,6 +33,48 @@ s1.sendall("You are going to play with ".encode()+name2.encode())
 
 
 print("Players:"+name1+" and "+name2)
+
+
+
+#### then you start a game
+
+s1.sendall("Rock/Paper/Scissors? Make sure you type it correctly".encode())
+
+p1=s1.recv(1024)
+
+p1=p1.decode()
+
+s2.send("Rock/Paper/Scissors? Make sure you type it correctly".encode())
+
+p2=s2.recv(1024)
+
+p2=p2.decode()
+
+if p1 == "Rock":
+    p1 = 1
+elif p1== "Paper":
+    p1 = 2
+elif p1 == "Scissors":
+    p1 = 3
+
+
+
+if p2 == "Rock":
+    p2 = 1
+elif p2 == "Paper":
+    p2 = 2
+elif p2 == "Scissors":
+    p2 = 3
+
+if p1 > p2:
+    s1.send("You won!!".encode())
+    s2.send("You lose!".encode())
+elif p1 < p2:
+    s2.send("You won!!".encode())
+    s1.send("You lose!!".encode())
+elif p1 == p2:
+    s1.send("You draw!!".encode())
+    s2.send("You draw!!".encode())
 
 s1.close()
 s2.close()
