6333b9aa2c940c3f8eb79d70d1c165c3160b130d
Adel Fergatova <68508788+adelfergatova@users.noreply.github.com>
2020-07-22 21:56:03 +0600

    Update rps-2player-server-final.py

--- rps-2player-server-final.py
+++ rps-2player-server-final.py
@@ -2,11 +2,11 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 9030))
-s.listen()
+s.listen()                                                        # server is ready for connections (it is listening)
 
 s1, addr1 = s.accept()
 
-s1.sendall("Hello! Welcome to our game!\n".encode())
+s1.sendall("Hello! Welcome to our game!\n".encode())                  # messages that we send to the first player
 
 s1.sendall("What is your username? Please enter:  ".encode())
 
@@ -14,11 +14,11 @@ data = s1.recv(1024)
 
 data = data.decode()
 
-name1=data
+name1=data                                                          # now we have the username of the first player
 
 s2, addr2 = s.accept()
 
-s2.sendall("Hello! Welcome to our game! You are going to play with ".encode()+ name1.encode())
+s2.sendall("Hello! Welcome to our game! You are going to play with ".encode()+ name1.encode())   # the second player is connected now)
 
 s2.sendall("What is your username? Please enter:  ".encode())
 
@@ -27,7 +27,7 @@ data = s2.recv(1024)
 
 data2 = data.decode()
 
-name2=data2
+name2=data2                                                                      # now we know the username of the second player
 
 s1.sendall("You are going to play with ".encode()+name2.encode())
 
@@ -44,7 +44,7 @@ p1=s1.recv(1024)
 
 p1=p1.decode()
 
-list=["Rock","Scissors","Paper"]
+list=["Rock","Scissors","Paper"]                                                              # if there is a mistake in spelling or wrong input
 if p1 in list:
     s1.sendall("Waiting for your opponent's choice \n".encode())
 else:
@@ -89,6 +89,7 @@ elif p2 == "Paper":
 elif p2 == "Scissors":
     p2 = 3
 
+#https://stackoverflow.com/questions/43312404/trying-to-make-a-2-player-rock-paper-scissors-game - we created the code based on this websites information, but we really made changes in it
 if p1 == p2:
     s1.send("You draw!!".encode())
     s2.send("You draw!!".encode())
